d46f4a64c8173dbedd7900b90e72b7f17cd8f656
Prince90901 <93855916+Prince90901@users.noreply.github.com>
2022-01-05 23:41:15 +0530

    Delete form.py

--- form.py
+++
@@ -1,8 +0,0 @@
-print("Content-type: text/html\n\n")
-import cgi
-import cgitb; cgitb.enable()
-form = cgi.FieldStorage()
-fname  = form['firstName']
-with open('mail.txt', 'a') as k:
-    k.write(f'{fname}')
-print('your form has been submitted')
